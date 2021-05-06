8b90cba31555c80424c583872513f1d944c46d73
Adarsh Raghav <aadarshraghav@gmail.com>
2021-05-06 16:24:35 +0530

    Added Favourites Functionality

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink(destination: LandmarkDetails(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
                 }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination:LandmarkDetails(landmark: landmark)) {
+                                    LandmarkRow(landmark: landmark)
+                                }
+                            }
             }
             .navigationTitle("Landmarks")
         }
@@ -22,10 +33,6 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
             LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
         }
-    }
 }
